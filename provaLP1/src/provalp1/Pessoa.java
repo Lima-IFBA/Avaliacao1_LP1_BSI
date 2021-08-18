5edc03269d5f6c0e5495b24c9cd32e4646fe18de
Gustavo Lima Cabral <79602409+l1ma-gustaf@users.noreply.github.com>
2021-08-17 22:15:53 -0300

    Add files via upload

---
+++ provaLP1/src/provalp1/Pessoa.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package provalp1;
+
+/**
+ *
+ * @author gusta
+ */
+public class Pessoa {
+    private String nome;
+    private int idade;
+    private double altura;
+    
+    
+    public String getNome(){
+        return nome;
+    }
+    public int getIdade(){
+        return idade;
+    }
+    public double getAltura(){
+        return altura;
+    }
+     public void setNome(String _nome){
+         nome = _nome;
+     }
+     public void setIdade(int _idade){
+         idade = _idade;
+     }
+     public void setAltura(double _altura){
+         altura = _altura;
+     }
+     
+     public String dados(){
+         return "Nome: " + nome + "\n" + "Idade: " + idade + " ano(s)" + "\n" + "Altura: " + altura + " metro(s)";
+     }
+}
